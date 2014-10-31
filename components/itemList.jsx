15ba54d7015f8ca2139331036f5d6f692f56d099
andy <andy@andydote.co.uk>
2014-10-31 01:52:48 +0000

    itemList: change css class used

--- components/itemList.jsx
+++ components/itemList.jsx
@@ -4,7 +4,7 @@ var ItemList = React.createClass({
 
   render: function() {
     return (
-      <div className="templateList large-4 column">
+      <div className="itemList large-4 column">
         {this.props.items}
       </div>
     );
