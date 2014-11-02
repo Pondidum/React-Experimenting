0ebdec9ee5e9b13de72a857400e12367e5bd6d13
andy <andy@andydote.co.uk>
2014-11-02 16:43:24 +0000

    component.templateItem: style it

--- components/templateItem.jsx
+++ components/templateItem.jsx
@@ -4,15 +4,21 @@ var TemplateItem = React.createClass({
   render: function() {
     var template = this.props.template;
 
+    var brands = template.brands.map(function(brand) {
+      return (
+        <a href="#">[{brand}]</a>
+      );
+    });
+
     return (
       <div className="templateItem">
         <h1>{template.name}</h1>
-        <p>{template.description}</p>
-        <p>Brands: {template.brands}</p>
-        <ul>
+        <ul className="actions">
           <li><a href="#">View/Edit</a></li>
           <li><a href="#">History</a></li>
         </ul>
+        <p className="description">{template.description}</p>
+        <div className="brands">Brands: {brands}</div>
       </div>
     );
   }
