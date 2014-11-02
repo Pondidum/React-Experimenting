97a2172d5fed7c01b2acd032b581536b28999ff9
andy <andy@andydote.co.uk>
2014-11-02 15:58:55 +0000

    landingPage: no header

--- components/landingPage.jsx
+++ components/landingPage.jsx
@@ -4,7 +4,6 @@ var LandingPage = React.createClass({
   render: function() {
     return (
       <div className="row fullwidth">
-      	<Header />
         <TemplateList url="templates.json" interval={5000} />
         <HistoryList url="history.json" interval={5000} />
         <div className="column end" />
