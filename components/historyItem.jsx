edc89e7a2b161d26fde8d1609f34d6b5f7145156
andy <andy@andydote.co.uk>
2014-11-02 23:03:11 +0000

    history: style history items

--- components/historyItem.jsx
+++ components/historyItem.jsx
@@ -2,14 +2,16 @@
 
 var HistoryItem = React.createClass({
   render: function() {
+
     var entry = this.props.entry;
+    var date = moment(entry.createDate).calendar();
 
     return (
       <div className="historyItem">
         <img src={entry.userIcon} />
+        <p className="timestamp">{date}</p>
         <h1>{entry.userName}</h1>
-        <p>{entry.createData}</p>
-        <a href="#">{entry.templateName} [{entry.templateBrand}]</a>
+        <a className="action" href="#">{entry.templateName} [{entry.templateBrand}]</a>
       </div>
     );
   }
