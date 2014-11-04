f6b49281b7aee41e92bf52d824725f443f63dfa3
andy <andy@andydote.co.uk>
2014-11-04 18:52:26 +0000

    header: style for editor page

--- components/header.jsx
+++ components/header.jsx
@@ -3,19 +3,9 @@
 var Header = React.createClass({
   render: function() {
     return (
-      <nav className="top-bar">
-        <ul className="title-area">
-          <li className="name">
-            <h1><a href="#">My Site</a></h1>
-          </li>
-          <li className="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-        </ul>
-        <section className="top-bar-section">
-          <ul className="left">
-            <li><a href="#">Left Nav Button</a></li>
-          </ul>
-        </section>
-      </nav>
+      <div className="header row fullwidth">
+        <a href="#">&lt; Back</a>
+      </div>
     );
   }
 });
